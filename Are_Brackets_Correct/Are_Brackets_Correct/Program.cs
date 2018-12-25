3fc44a9f5db612ae4db11ca37b1e2f24a71b64b3
annie .h.h <annie2016l@gmail.com>
2018-12-25 21:42:30 +0400

    Are_Brackets_Correct

---
+++ Are_Brackets_Correct/Are_Brackets_Correct/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    class Program
+    {
+        public static void IsBracketCorrect(string text)
+        {
+            string brackets = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(' || text[i] == '{' || text[i] == '[' || text[i] == '<' ||
+                    text[i] == ')' || text[i] == '}' || text[i] == ']' || text[i] == '>')
+                {
+                    brackets += text[i];
+                }
+            }
+
+            for (int i = 0; i < brackets.Length; i++)
+            {
+                if (brackets.Contains("()") || brackets.Contains("[]") || brackets.Contains("{}") || brackets.Contains("<>"))
+                {
+                    brackets = brackets.Replace("()", "");
+                    brackets = brackets.Replace("{}", "");
+                    brackets = brackets.Replace("[]", "");
+                    brackets = brackets.Replace("<>", "");
+                }
+            }
+
+            if (brackets == "")
+            {
+                Console.WriteLine("All brackets are correct.");
+            }
+            else
+            {
+                Console.WriteLine("Brackets are not correct.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            IsBracketCorrect(text);
+            Console.ReadKey();
+        }
+    }
+}
