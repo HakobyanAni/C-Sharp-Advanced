ad44fce9a1eec85bf76b26de3c3d69f7d511d440
annie .h.h <annie2016l@gmail.com>
2019-01-04 16:52:51 +0400

    Delegate/Anonymous_Method

---
+++ Friend's_Mood(Delegates, Anonymous_Methods)/Friend's_Mood(Delegates, Anonymous_Methods)/Friend.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends_Mood
+{
+    public class Friend
+    {
+        private string firstName;
+        private string lastName;
+        private int friendAge;
+        public Moods Mood { get; set; }
+        public Print SayHello { get; set; }
+
+        public Friend(string firstName, string lastName, int friendAge)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.friendAge = friendAge;
+            Mood = Moods.Excellent;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public int FriendAge
+        {
+            get { return friendAge; }
+            set { friendAge = value; }
+        }
+
+        public void PrintMyMood()
+        {
+            Console.WriteLine($"My mood is {Mood}");
+        }
+
+        public override string ToString()
+        {
+            return $"My name is {firstName} {lastName}. I am {friendAge} years old. My mood is {Mood}";
+        }
+    }
+}
---
+++ Friend's_Mood(Delegates, Anonymous_Methods)/Friend's_Mood(Delegates, Anonymous_Methods)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends_Mood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Friend friend = new Friend("Sherlock", "Holmes", 35);
+                Console.Write($"I am {friend.FirstName} {friend.LastName} Select my mood: Bad - 1, Normal - 2, Good - 3, Excellent - 4\nMood = ");
+
+                int choosenMoodInInteger = Convert.ToInt32(Console.ReadLine());
+                friend.Mood = (Moods)choosenMoodInInteger;
+
+                switch (friend.Mood)
+                {
+                    case Moods.Bad:
+                        friend.SayHello = delegate { Console.WriteLine("Hello Stranger."); };
+                        break;
+                    case Moods.Normal:
+                        friend.SayHello = delegate { Console.WriteLine("Hello Brother."); };
+                        break;
+                    case Moods.Good:
+                        friend.SayHello = () => Console.WriteLine("Hello my dear. How are you?");
+                        break;
+                    case Moods.Excellent:
+                        friend.SayHello = delegate { Console.WriteLine("Hello my dear. I miss you."); };
+
+                        break;
+                    default:
+                        break;
+                }
+
+                friend.PrintMyMood();
+                friend.SayHello();
+
+                Console.WriteLine("\n--------------------------------------------");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
