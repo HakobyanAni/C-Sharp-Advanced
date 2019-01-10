d332e7021c95277d3e0eaa07766269b2b778b520
annie .h.h <annie2016l@gmail.com>
2019-01-10 21:52:45 +0400

    Reflection

---
+++ Solar_System_About_Reflection/Solar_System_About_Reflection/ClassLibrary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public enum Planets
+    {
+        Mercury = 1,
+        Venus,
+        Earth,
+        Mars,
+        Jupiter,
+        Saturn,
+        Uranus,
+        Neptune
+    }
+
+    public class SolarSystem
+    {
+        private Planets PlanetName { get; set; }
+        private int PlaceInGalaxy { get; set; }
+
+        private SolarSystem(Planets planetName, int placeInGalaxy)
+        {
+            PlanetName = planetName;
+            PlaceInGalaxy = placeInGalaxy;
+        }
+
+        public SolarSystem()
+        {
+
+        }
+        private void RotatePlanet(Planets planetName)
+        {
+            Console.WriteLine($"{planetName} rotates around the sun.");
+        }
+    }
+}
---
+++ Solar_System_About_Reflection/Solar_System_About_Reflection/Program.cs
@@ -0,0 +1,78 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void ListOfMethods(Type type)
+        {
+            MethodInfo[] methodInfos = type.GetMethods(System.Reflection.BindingFlags.Instance
+                     | BindingFlags.Static
+                     | BindingFlags.Public
+                     | BindingFlags.NonPublic
+                     | BindingFlags.DeclaredOnly); // Getting all information about methods of the class SolarSystem
+
+            Console.WriteLine("--> Methods and their types ");
+            foreach (MethodInfo method in methodInfos)
+            {
+                Console.WriteLine($"{method.Name} - {method.ReturnType}");
+            }
+        }
+
+        static void ListOfFields(Type type)
+        {
+            FieldInfo[] fieldInfos = type.GetFields(System.Reflection.BindingFlags.Instance
+                                    | BindingFlags.Static
+                                    | BindingFlags.Public
+                                    | BindingFlags.NonPublic);  // Getting all information about fields of the class SolarSystem
+
+            Console.WriteLine("-> Field Names And Types");
+            foreach (FieldInfo field in fieldInfos)
+            {
+                Console.WriteLine($"              { field.Name} - {field.FieldType}");
+            }
+        }
+
+        static void ListOfConstructors(Type type)
+        {
+            ConstructorInfo[] constructorInfos = type.GetConstructors(System.Reflection.BindingFlags.Instance
+                                                | BindingFlags.Static
+                                                | BindingFlags.Public
+                                                | BindingFlags.NonPublic); // Getting all information about constructors of the class SolarSystem
+
+            Console.WriteLine("-> Constructors");
+            Console.WriteLine("IsPrivate     IsStatic     IsPublic");
+            foreach (ConstructorInfo constructor in constructorInfos)
+            {
+                Console.WriteLine($"    {constructor.IsPrivate}   -   {constructor.IsStatic}   -   {constructor.IsPublic}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            SolarSystem solarSystem = new SolarSystem();  // Creating an instance of the class SolarSystem
+
+            Type type = solarSystem.GetType();  // Getting the type of class SolarSystem
+                                                // or
+                                                // Type type = typeof(SolarSystem);
+
+            ListOfMethods(type);
+            Console.WriteLine("------------------------------------------------------------");
+
+            ListOfFields(type);
+            Console.WriteLine("------------------------------------------------------------");
+
+            ListOfConstructors(type);
+            Console.WriteLine("------------------------------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
